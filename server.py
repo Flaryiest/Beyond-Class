4c56df5f15783e05b2e8c57e2f74b45fe361a6c0
jerrbearis2cool <68572439+jerrbearis2cool@users.noreply.github.com>
2024-10-05 09:50:17 -0600

    Added email verification

--- server.py
+++ server.py
@@ -1,13 +1,53 @@
-import os, json, psycopg2, time, random
+import os, json, psycopg2, time, random, uuid, hashlib, smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 from math import ceil
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 from openai import OpenAI
-import hashlib
 
 load_dotenv() 
 
-DB_HOST = "127.0.0.1"
+tokens = {}
+EMAIL, PASSWORD = [os.environ.get(i) for i in ["EMAIL", "PASSWORD"]]
+
+def get_base(email):
+    local_part, domain = email.split('@')
+    normalized_local = local_part.split('+')[0].replace('.', '')
+    base_email = f'{normalized_local}@{domain}'
+    
+    return base_email
+
+def send_email(recipient, key):
+    try:
+        msg = MIMEMultipart('alternative')
+
+        html = f"""
+        <html>
+        <body>
+        <div style="display: flex; align-items: center; justify-content: center; border-radius: 10px; border-style: groove; padding-left: 50px; padding-right: 50px; padding-bottom: 50px;">
+            <div style="justify-content: center; align-items: center;">
+                <h1>BeyondClass - Register</h1>
+                <p>Hello! Click the link below to register your account!</p><br>
+                <p>If this was not you, please close this email.</p><br>
+            <a style="padding: 10px; border-radius: 0; background-color: white; border-style: solid; color: #000; text-decoration: none;" href="http://184.64.116.12:3333/verify?code={key}">Click here to register</a>
+            </p>
+        </body>
+        </html>
+        """
+        msg.attach(MIMEText(html, 'html'))
+        
+        msg['Subject'] = "TinyDrive farmer confirmation"
+        msg['From'] = EMAIL
+        msg['To'] = ', '.join([recipient])
+
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+            smtp_server.login(EMAIL, PASSWORD)
+            smtp_server.sendmail(EMAIL, [recipient], msg.as_string())
+
+        return True
+    except Exception:
+        return False
 
 class Database:
     def __init__(self, table, DB_NAME="postgres", DB_USER="postgres", DB_HOST="127.0.0.1", DB_PORT="5432"):
@@ -54,6 +94,11 @@ class Database:
             cur.execute(f'SELECT username FROM {self.table}')
             return cur.fetchall()
     
+    def num_users(self):
+        with self.conn.cursor() as cur:
+            cur.execute(f'SELECT COUNT(*) FROM {self.table}')
+            return cur.fetchone()[0]
+
     def verify(self, username):
         for user in self.get_keys():
             if username.lower() == user.lower():
@@ -64,6 +109,12 @@ class Database:
         with self.conn.cursor() as cur:
             cur.execute(f'SELECT data FROM {self.table}')
             return cur.fetchall()
+        
+    def check_token(self, username, token):
+        if self.get_data(username)["token"] == token:
+            return True
+        else:
+            return False
 
 class ChatGPT:
     def __init__(self, database):
@@ -129,7 +180,6 @@ class ChatGPT:
         except KeyError:
             return None
 
-
     def generate_quizzes(self, username, course, epoch):
         days = ceil((epoch - time.time()) / 86400)
         quizzes = {}
@@ -144,15 +194,29 @@ class ChatGPT:
         return quizzes
 
 
-database = Database("main", DB_HOST=DB_HOST)
+database = Database("main")
 database.create_table()
 
 chatgpt = ChatGPT(database)
 
 app = Flask(__name__)
 
-@app.route("/get_courses/<username>", methods=["GET"])
+@app.route("/get_courses", methods=["GET"])
 def get_courses(username):
+    """
+    Client JSON:
+    {
+        username: str # the username of the user
+        token:str # given token from login
+        password: str # the password of the user. ideally, should also be hashed by the client 
+    }
+    """
+    data = request.get_json(force=True)
+    username, token = [data[i] for i in ["username", "token"]]
+
+    if not database.check_token(username, token):
+        return jsonify({"success": False, "reason": "invalid token"})
+
     courses = []
     user_data = database.get_data(username)["courses"]
     for course in user_data:
@@ -160,24 +224,31 @@ def get_courses(username):
 
     return jsonify(database.get_data(username)["courses"])
 
+@app.route("/n_users", methods=["GET"])
+def n_users():
+    try:
+        return jsonify({"success": True, "n_users": database.num_users()})
+    except Exception:
+        return jsonify({"success": False})
+
 @app.route("/register", methods=["POST"])
 def register():
     data = request.get_json(force=True)
 
     """
-    Client JSON Data to send:
+    Client JSON:
     {
         username: str # the username of the user
+        email:str # email for verification
         password: str # the password of the user. ideally, should also be hashed by the client 
     }
     """
 
     # Check if the data is correct
-    if not ('username' in data and 'password' in data):
+    if not ('username' in data and 'password' in data and 'token' in data):
         return jsonify({"success": False, "reason": "Invaild JSON data"})
  
-    username = data['username']
-    password = data['password']
+    username, password, email = [data[i] for i in ["username", "password", "email"]]
 
     # Hash the password
     salt = hashlib.sha512(random.randbytes(64)).hexdigest()
@@ -185,12 +256,21 @@ def register():
     password = hashlib.sha256(bytes(password + salt, 'utf-8')).hexdigest()
 
     if database.verify(username):
-        database.insert_data(username, {"courses": {}, "password": password, "salt": salt})
+        token = hashlib.sha512(random.randbytes(64)).hexdigest()
+        tokens[token] = {"username": username, "password": password, "salt": salt}
+        
+        send_email(email, token)
 
         return jsonify({"success": True})
     
     else:
         return jsonify({"success": False, "reason": "Username is already registered in the database!"})
+
+@app.route("/verify", methods=["POST"])
+def verify():
+    token = request.get_json(force=True)["token"]
+    if token in tokens:
+        database.insert_data(tokens[token]["username"], {"courses": {}, "password": tokens[token]["password"], "salt": tokens[token]["salt"], "token": ""})
 
 @app.route('/login', methods=["POST"])
 def login():
@@ -203,21 +283,16 @@ def login():
         if not ('username' in data and 'password' in data):
             return jsonify({"success": False, "reason": "Invaild JSON data"})
 
-
-        userData = database.get_data(username)
-
-        # Hash the password
-        password = hashlib.sha256(bytes(data['password'] + userData['salt'], 'utf-8')).hexdigest()
-
+        user_data = database.get_data(username)
         username = data['username']
 
-        # Verify Password. you should create a function in the DB
-        correctPassword = userData['password']
+        if user_data['password'] == hashlib.sha256(bytes(data['password'] + user_data['salt'], 'utf-8')).hexdigest():
+            token = hashlib.sha512(random.randbytes(64)).hexdigest()
 
-        if correctPassword == password:
-            """some login thing like a session token or wutever."""
+            user_data["token"] = token
+            database.insert_data(username, user_data)
 
-            return jsonify({"success": True, })
+            return jsonify({"success": True, "token": token})
 
         else:
             return jsonify({"success": False, "reason": "Incorrect password!"})
